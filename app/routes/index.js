f733b502754b7cc0662f90ce7884894083b75537
Andreea Dumitru <Andreea.Dumitru@iquestgroup.com>
2019-07-15 15:51:59 +0300

    Add Mirage for creating fake data and mimic an API

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,7 +1,7 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
-    beforeModel() {
-        this.replaceWith('rentals');
+    redirect() {
+        this.transitionTo('rentals');
     }
 });
---
+++ tests/acceptance/list-rentals-test.js
@@ -0,0 +1,17 @@
+import { module, test } from 'qunit';
+import { visit, currentURL } from '@ember/test-helpers';
+import { setupApplicationTest } from 'ember-qunit';
+
+module('Acceptance | list rentals', function(hooks) {
+  setupApplicationTest(hooks);
+
+
+  test('should show rentals as the home page', async function (assert) {
+    await visit('/');
+    assert.equal(currentURL(), '/rentals', 'should redirect automatically');
+  });
+  test('should list available rentals.', async function(assert) {
+    await visit('/');
+    assert.equal(this.element.querySelectorAll('.listing').length, 3, 'should display 3 listings');
+  });
+});
